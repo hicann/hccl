b524f5a5946bd98216abe0cc55a8a69137355593
sqw <shiqiwen@huawei.com>
2026-06-26 10:58:21 +0800

    主线反合9.1.0分支：allgather cleancode && CCU样例 bugfix
    Co-authored-by: sqw<shiqiwen@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1551 merge 9.1.0 into 9.1.0
    
    主线反合9.1.0分支：allgather cleancode && CCU样例 bugfix
    
    Created-by: sqw
    Commit-by: sqw
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hccl!1551

--- src/ops/all_gather/template/ccu/ccu_temp_all_gather_nhr_1D_mem2mem.h
+++ src/ops/all_gather/template/ccu/ccu_temp_all_gather_nhr_1D_mem2mem.h
@@ -51,6 +51,11 @@ private:
     HcclResult GetStepInfo(u32 step, u32 nSteps, NHRStepInfo &stepInfo);
     HcclResult SplitDataFor2Dies(const OpParam& param, const TemplateDataParams& templateDataParams, uint64_t& die0Size,
                                  uint64_t& die1Size) const;
+    HcclResult BuildCcuKernelInfos(const OpParam& param, uint32_t dieNum, uint32_t kernelNum,
+                                   const std::vector<NHRStepInfo>& stepInfoVector,
+                                   const std::map<u32, u32>& rank2ChannelIdx,
+                                   const std::vector<std::vector<HcclChannelDesc>>& channelsPerDie,
+                                   AlgResourceRequest& resourceRequest);
     HcclResult PrepareLaunchArgs(const OpParam& param, const TemplateDataParams& templateDataParams, u32 kernelNum,
                                  std::vector<uint64_t>& taskArgs, uint64_t& argSize);
 };
