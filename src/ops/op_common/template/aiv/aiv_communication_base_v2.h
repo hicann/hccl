4a89a9bc693d1523820648337eb2258c30bd8c8c
shirokawa_ <jinliuyi@huawei.com>
2026-07-26 16:05:01 +0800

    AIV SK allreduce算法流程统一 + clearSyncBuf修改
    Co-authored-by: shirokawa_<jinliuyi@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2260 merge main0726 into master
    
    AIV SK allreduce算法流程统一 + clearSyncBuf修改
    
    Created-by: shirokawa_
    Commit-by: shirokawa_
    Merged-by: cann-robot
    Description: ## 描述
    AIV SK allreduce算法流程统一 + clearSyncBuf修改
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/hccl!2260

--- src/ops/op_common/template/aiv/aiv_communication_base_v2.h
+++ src/ops/op_common/template/aiv/aiv_communication_base_v2.h
@@ -447,11 +447,7 @@ __aicore__ inline void AivCommBase::Record(uint32_t targetRank, uint64_t flag_of
 
 __aicore__ inline void AivCommBase::ClearSyncBuf()
 {
-    // 用10个flag
-    Barrier(1);
-    ClearFlag();
-    Barrier(DOUBLE);
-    SyncAll<true>();
+    BarrierForFirstOP();
 }
 
 __aicore__ inline void AivCommBase::Barrier(uint32_t step)
