7f38f5eb3d22ffe396b985496fa6a360b804bbfe
silencesirius <caixin21@huawei.com>
2026-07-30 20:00:02 +0800

    适配PCIE-SW机型在3层拓扑下建PCIE链功能
    Co-authored-by: silencesirius<caixin21@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2365 merge 9.2.0-beta.1 into 9.2.0-beta.1
    
    适配PCIE-SW机型在3层拓扑下建PCIE链功能
    
    Created-by: silencesirius
    Commit-by: silencesirius
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    1.适配在netlayer3存在的情况下，16p PCIE-SW机型能够正确建PCIE链路，并走入Parallel算法。
    2.适配reduce类int64数据类型在16p PCIE-SW机型下能够正确走入aiv模式。
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    PCIE-SW机型，16P/48P/64P场景，测试了AllReduce、ReduceScatter和AllGather算子。
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/hccl!2365

--- src/ops/op_common/selector/auto_selector_base.h
+++ src/ops/op_common/selector/auto_selector_base.h
@@ -99,6 +99,7 @@ public:
                                    const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
                                    std::string &selectAlgName) const;
     bool IsStarsState(const OpExecuteConfig &opExecuteConfig) const;
+    bool IsRollBackAiv(OpParam &opParam, TopoInfoWithNetLayerDetails* topoInfo) const;
     bool IsLayerAllConnetedWithTopo(const TopoInfoWithNetLayerDetails *topoInfo, const u32 netLayer, const CommTopo topoType) const;
     HcclResult CheckMeshNumEqualToClosNum(const TopoInfoWithNetLayerDetails *topoInfo, bool &isEqual) const;
     HcclResult CheckClosNumMultipleOfMeshNum(const TopoInfoWithNetLayerDetails *topoInfo, bool &isMultiple) const;
