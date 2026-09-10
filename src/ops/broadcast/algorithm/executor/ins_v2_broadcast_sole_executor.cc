3289800dca0c79d0c95e3729efec329b001a6633
hblnb <huangbolin3@huawei.com>
2026-09-10 10:07:37 +0800

    fix server channels
    Co-authored-by: hblnb<huangbolin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2974 merge fix_reduce_scatter into master
    
    fix server channels
    
    Created-by: hblnb
    Commit-by: hblnb
    Merged-by: cann-robot
    Description: ## 描述
    之前多jetty的需求将SetchannelsPerRank的执行条件从算法名判断改成了isPod，导致server环境上算子执行有问题。现在改成isPod或者算法名判断，这样pod环境不判断算法进入，满足pod环境下各机出一卡也能使用多jetty，同时server和之前的逻辑一样
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    RDV RATIO:93.33%，失败用例为已知问题
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/hccl!2974

--- src/ops/broadcast/algorithm/executor/ins_v2_broadcast_sole_executor.cc
+++ src/ops/broadcast/algorithm/executor/ins_v2_broadcast_sole_executor.cc
@@ -182,7 +182,8 @@ HcclResult InsV2BroadcastSoleExecutor<AlgTopoMatch, InsAlgTemplate>::Orchestrate
     // 构建template
     std::shared_ptr<InsAlgTemplate> algTemplate
         = std::make_shared<InsAlgTemplate>(param, resCtx.topoInfo.userRank, resCtx.algHierarchyInfo.infos[0]);
-    if (param.engine == CommEngine::COMM_ENGINE_AICPU_TS && resCtx.topoInfo.isPod) {
+    if (param.engine == CommEngine::COMM_ENGINE_AICPU_TS
+        && (resCtx.topoInfo.isPod || std::string(param.algName) == "AicpuBroadcastSoleNHRMultiLink")) {
         CHK_RET(algTemplate->SetchannelsPerRank(templateAlgRes.channels));
     }
 
